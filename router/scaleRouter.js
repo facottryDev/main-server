c7ba24a8b2f99fe1326eaf902b914468b455aac7
Styles <bcs_2021035@iiitm.ac.in>
2024-06-24 21:43:25 +0530

    Update scaleRouter.js

--- router/scaleRouter.js
+++ router/scaleRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getMapping } from "../controllers/scale";
+import { getMapping } from "../controllers/scale.js";
 const router = Router();
 
 router.post("/get-mappings", getMapping);
